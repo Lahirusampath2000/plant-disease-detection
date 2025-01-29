3a95eab4f7968ab6f5f3d566276ef39e518673b4
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-01-29 22:42:33 +0530

    form position change

--- frontend/src/components/imageUpload.js
+++ frontend/src/components/imageUpload.js
@@ -2,21 +2,38 @@ import React from "react";
 
 function ImageUpload() {
   return (
-    <div style={{ textAlign: "center", padding: "20px" }}>
-        <div className="container"></div>
-        <h1>Upload an Image</h1>
-      <form method="post" encType="multipart/form-data" >
-        <label for="image">Select an image:</label>
-        <br/>
-        <br/>
-        <input class="form-control" type="file" id="formFile" />
-        <br/>
-        <br/>
-        <button type="submit" class="btn btn-primary">Submit</button>
-        
-        
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "flex-start", // You can set this to "flex-start" or "center"
+        padding: "20px",
+        position: "relative",
+      }}
+    >
+        <div className="container" 
+        style={{
+          position: "absolute", // You can use absolute positioning for more control
+          left: "65%",
+          top: "300%", 
+        }}>
+            <h1>Upload an Image</h1>
+            <br/>
+            <br/>
+            <form method="post" encType="multipart/form-data" >
+                <label for="image">Select an image:</label>
+                <br/>
+                <br/>
+                <input class="form-control" type="file" id="formFile" style={{ width: "20%" }} />
+                <br/>
+                <br/>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                
+            
 
-      </form>
+            </form>
+
+        </div>
+       
       
       
     </div>
