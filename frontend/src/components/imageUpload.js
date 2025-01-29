6c272c97e00e7fac96de1157598e4431d7bbe42d
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-01-29 22:25:36 +0530

    added bootstrap class

--- frontend/src/components/imageUpload.js
+++ frontend/src/components/imageUpload.js
@@ -3,18 +3,21 @@ import React from "react";
 function ImageUpload() {
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
-      <h1>Upload an Image</h1>
+        <div className="container"></div>
+        <h1>Upload an Image</h1>
       <form method="post" encType="multipart/form-data" >
         <label for="image">Select an image:</label>
+        <br/>
         <br/>
         <input type="file" id="image" name="image" accept="image/*" />
         <br/>
         <br/>
-        <button type="submit">Submit</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
         
         
 
       </form>
+      
       
     </div>
   );
