9ed4376017ef1eacfec3cb08868a51df70c11e80
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-03-08 12:03:46 +0530

    secret key added

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 class ApplicationConfig:
+    SECRET_KEY = os.environ('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
