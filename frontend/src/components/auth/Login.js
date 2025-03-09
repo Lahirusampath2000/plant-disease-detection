1f80651a846268cf3794bfae641409afd1dafe42
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-03-09 12:43:00 +0530

    login page

---
+++ frontend/src/components/auth/Login.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://127.0.0.1:5000/login", {
+        name,
+        email,
+        password,
+      }, {
+        headers: {
+          "Content-Type": "application/json",  // Ensure the content type is set to JSON
+        },
+      });
+
+      alert(response.data.message);
+      navigate("/"); // Redirect to Login page after registration
+    } catch (error) {
+      alert("Error during registration!");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h2>LOGIN</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-dark mt-4">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
