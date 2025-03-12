c7c23cea6ac178b3f732e2ccb37b08de03aac38e
Lahiru sampath <lahiru.20210981@iit.ac.lk>
2025-03-12 16:51:28 +0530

    btn position chng

--- frontend/src/components/imageUpload.js
+++ frontend/src/components/imageUpload.js
@@ -226,8 +226,8 @@ const ImageUpload = () => {
               onClick={closePopup}
               style={{
                 position: "absolute",
-                top: "10px",
-                right: "15px",
+                top: "0",
+                right: "0",
                 background: "none",
                 border: "none",
                 fontSize: "20px",
